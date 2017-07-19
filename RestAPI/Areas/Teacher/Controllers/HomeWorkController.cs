6e7599a962d7da4ac22cdeeb62809fdeaf3b6a90
jiang yuanqing <jiang_yuanqing@126.com>
2017-07-19 17:32:22 +0800

    提交

---
+++ RestAPI/Areas/Teacher/Controllers/HomeWorkController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RestAPI.Areas.Teacher.Controllers
+{
+    //[RoutePrefix("teacher/homework")]
+    public class HomeWorkController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "getall";
+        }
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "get_" + id;
+        }
+
+
+        public void Delete(int id)
+        {
+
+        }
+
+        public string Put(int id)
+        {
+            return "put_" + id;
+        }
+
+        public string Post(int id)
+        {
+            return "post_" + id;
+        }
+    }
+}
